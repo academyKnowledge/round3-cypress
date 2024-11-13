9c68fcb69cb03a8dccc36595039d811413e20ea6
academyKnowledge <academyk74@gmail.com>
2024-11-13 20:55:16 +0200

    js file

---
+++ cypress/functions.js
@@ -0,0 +1,18 @@
+// functions 
+let result;
+
+function myFunction(p1,p2){
+	return p1*p2;
+}
+console.log(myFunction(2,5));
+
+hello = function(p1,p2){
+	return p1*p2;
+}
+console.log(hello(2,5));
+
+// arrow function 
+hello = (p1,p2)=>{
+return p1*p2
+}
+console.log(hello(6,5));
